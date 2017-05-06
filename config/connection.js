af2729b4f108a494b7fef0f61da7739e41bd1c7b
Jackie Wellons <jackie@localhost.localdomain>
2017-05-06 10:01:27 -0700

    quick update

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if(process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "pChrms1115",
+	password: "",
 	database: "burgers_db"
     });
 };
